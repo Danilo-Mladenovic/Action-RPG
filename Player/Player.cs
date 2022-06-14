3a51f145f281f54417ca7acc9caa1a5ac3bbf8d8
Danilo <danilo.mladenovic999@gmail.com>
2022-06-14 14:46:59 +0200

    Added basic player movement.

---
+++ Player/Player.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody2D
+{
+
+    private Vector2 velocity;
+
+    public override void _Ready()
+    {
+        velocity = Vector2.Zero;
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        Vector2 inputVector = Vector2.Zero;
+        inputVector.x = 2 * (Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left"));
+        inputVector.y = 2 * (Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up"));
+
+        if (inputVector != Vector2.Zero)
+            velocity = inputVector;
+        else
+            velocity = Vector2.Zero;
+
+        MoveAndCollide(velocity);
+    }
+}
