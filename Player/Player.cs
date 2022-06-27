c3ec3d841934fe26c156c3198fe2aea91be185cd
Danilo-Mladenovic <danilo.mladenovic999@gmail.com>
2022-06-27 08:36:00 +0200

    Made movement more smooth with delta multiplicaiton.

--- Player/Player.cs
+++ Player/Player.cs
@@ -5,6 +5,9 @@ public class Player : KinematicBody2D
 {
 
     private Vector2 velocity;
+    private int MAX_SPEED = 150;
+    private int ACCELERATION = 50;
+    private float FRICTION = 25;
 
     public override void _Ready()
     {
@@ -16,11 +19,15 @@ public class Player : KinematicBody2D
         Vector2 inputVector = Vector2.Zero;
         inputVector.x = 2 * (Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left"));
         inputVector.y = 2 * (Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up"));
+        inputVector = inputVector.Normalized();
 
         if (inputVector != Vector2.Zero)
-            velocity = inputVector;
+        {
+            velocity += inputVector * ACCELERATION * delta;
+            velocity = velocity.Clamped(MAX_SPEED * delta);
+        }
         else
-            velocity = Vector2.Zero;
+            velocity = velocity.MoveToward(Vector2.Zero, FRICTION * delta);
 
         MoveAndCollide(velocity);
     }
